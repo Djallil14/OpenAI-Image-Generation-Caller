a4f3f8ec95f3280ac86467574caf01ab3ff721f9
Djallil Elkebir <elkebir.djallil@icloud.com>
2023-02-08 20:21:23 -0500

    Initial Commit

---
+++ ImageAIGenerator/ImageAIGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAIGeneratorApp.swift
+//  ImageAIGenerator
+//
+//  Created by Djallil on 2023-02-08.
+//
+
+import SwiftUI
+
+@main
+struct ImageAIGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
